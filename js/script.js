243e818c718a9bb00ab196cc72384417a3e07f8c
Juan Pablo Dulce <86208054+ValperGT@users.noreply.github.com>
2021-10-19 17:13:54 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const $redInput = document.querySelector("#red-input");
+const $greenInput = document.querySelector("#green-input");
+const $blueInput = document.querySelector("#blue-input");
+const $alphaInput = document.querySelector("#alpha-input");
+const $redLabel = document.querySelector("#red-label");
+const $greenLabel = document.querySelector("#green-label");
+const $blueLabel = document.querySelector("#blue-label");
+const $alphaLabel = document.querySelector("#alpha-label");
+const $container = document.querySelector(".container");
+const body = document.body;
+
+const savedColor = JSON.parse(localStorage.getItem("color"));
+
+const color = {
+    "red":0,
+    "green":0,
+    "blue":0,
+    "alpha":1.0
+}
+
+if(savedColor){
+    $redLabel.innerText = savedColor.red;
+    $greenLabel.innerText = savedColor.green;
+    $blueLabel.innerText = savedColor.blue;
+    $alphaLabel.innerText = savedColor.alpha;
+    color.red = savedColor.red;
+    color.green = savedColor.green;
+    color.blue = savedColor.blue;
+    color.alpha = savedColor.alpha;
+    body.style.backgroundColor = `rgba(${savedColor.red}, ${savedColor.green}, ${savedColor.blue}, ${savedColor.alpha})`;
+}
+
+$redInput.addEventListener("input", ()=>{
+    $redLabel.innerText = $redInput.value;
+    color.red = $redInput.value;
+    body.style.backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+    localStorage.setItem("color", JSON.stringify(color));
+})
+
+$greenInput.addEventListener("input", ()=>{
+    $greenLabel.innerText = $greenInput.value;
+    color.green = $greenInput.value;
+    body.style.backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+    localStorage.setItem("color", JSON.stringify(color));
+})
+
+$blueInput.addEventListener("input", ()=>{
+    $blueLabel.innerText = $blueInput.value;
+    color.blue = $blueInput.value;
+    body.style.backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+    localStorage.setItem("color", JSON.stringify(color));
+})
+
+$alphaInput.addEventListener("input", ()=>{
+    $alphaLabel.innerText = $alphaInput.value;
+    color.alpha = $alphaInput.value;
+    body.style.backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+    localStorage.setItem("color", JSON.stringify(color));
+})
