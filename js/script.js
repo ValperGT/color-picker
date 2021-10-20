05f71e81946b53fba0e52a5de48afa8cc3cd0054
Juan Pablo Dulce <86208054+ValperGT@users.noreply.github.com>
2021-10-20 17:15:20 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,35 +7,35 @@ const $greenLabel = document.querySelector("#green-label");
 const $blueLabel = document.querySelector("#blue-label");
 const $alphaLabel = document.querySelector("#alpha-label");
 const $container = document.querySelector(".container");
+const $buttonClipboard = document.querySelector(".input__copy-button");
 const body = document.body;
 
-const savedColor = JSON.parse(localStorage.getItem("color"));
+const redRandomValue = Math.floor(Math.random() * (255 + 1));
+const greenRandomValue = Math.floor(Math.random() * (255 + 1));
+const blueRandomValue = Math.floor(Math.random() * (255 + 1));
 
 const color = {
-    "red":0,
-    "green":0,
-    "blue":0,
+    "red":redRandomValue,
+    "green":greenRandomValue,
+    "blue":blueRandomValue,
     "alpha":1
 }
 
-if(savedColor){
-    $redLabel.innerText = savedColor.red;
-    $greenLabel.innerText = savedColor.green;
-    $blueLabel.innerText = savedColor.blue;
-    $alphaLabel.innerText = savedColor.alpha;
-    color.red = savedColor.red;
-    color.green = savedColor.green;
-    color.blue = savedColor.blue;
-    color.alpha = savedColor.alpha;
-    body.style.backgroundColor = `rgba(${savedColor.red}, ${savedColor.green}, ${savedColor.blue}, ${savedColor.alpha})`;
-}
+body.style.backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
+$redLabel.innerText = color.red;
+$greenLabel.innerText = color.green;
+$blueLabel.innerText = color.blue;
+$alphaLabel.innerText = color.alpha;
+$redInput.value = color.red;
+$greenInput.value = color.green;
+$blueInput.value = color.blue;
+$alphaInput.value = color.alpha;
 
 $redInput.addEventListener("input", ()=>{
     $redLabel.innerText = $redInput.value;
     color.red = $redInput.value;
     let newColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
     body.style.backgroundColor = newColor;
-    localStorage.setItem("color", JSON.stringify(color));
 })
 
 $greenInput.addEventListener("input", ()=>{
@@ -43,7 +43,6 @@ $greenInput.addEventListener("input", ()=>{
     color.green = $greenInput.value;
     let newColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
     body.style.backgroundColor = newColor;
-    localStorage.setItem("color", JSON.stringify(color));
 })
 
 $blueInput.addEventListener("input", ()=>{
@@ -51,7 +50,6 @@ $blueInput.addEventListener("input", ()=>{
     color.blue = $blueInput.value;
     let newColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
     body.style.backgroundColor = newColor;
-    localStorage.setItem("color", JSON.stringify(color));
 })
 
 $alphaInput.addEventListener("input", ()=>{
@@ -59,5 +57,12 @@ $alphaInput.addEventListener("input", ()=>{
     color.alpha = $alphaInput.value;
     let newColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha})`;
     body.style.backgroundColor = newColor;
-    localStorage.setItem("color", JSON.stringify(color));
+})
+
+$buttonClipboard.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(`rgba(${color.red},${color.green},${color.blue},${color.alpha});`);
+    $buttonClipboard.classList.add("active");
+    setTimeout(()=>{
+        $buttonClipboard.classList.remove("active");
+    },2000)
 })
